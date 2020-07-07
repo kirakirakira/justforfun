1bdc768a8a727b2ed3f3332f9de256b9d753277f
Kira Hartlage <kira.hartlage@gmail.com>
2020-07-07 19:53:05 -0400

    added my info

--- content/blog/hello-world/index.md
+++ content/blog/hello-world/index.md
@@ -19,4 +19,6 @@ Oh, and here's a great quote from this Wikipedia on
 > salted duck eggs have a briny aroma, a gelatin-like egg white and a
 > firm-textured, round yolk that is bright orange-red in color.
 
+# This is a new header
+
 ![Chinese Salty Egg](./salty_egg.jpg)
--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Kira`,
+      summary: `who lives and works in her house and basically never leaves anymore.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `kirahartlage`,
     },
   },
   plugins: [
